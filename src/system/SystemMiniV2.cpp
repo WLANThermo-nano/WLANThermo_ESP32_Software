0f985e57a5e10c19a2eab876332c5d660a291b36
tuniii <23635855+tuniii@users.noreply.github.com>
2020-03-19 21:13:43 +0100

    Add pitmaster profiles dynamically

--- src/system/SystemMiniV2.cpp
+++ src/system/SystemMiniV2.cpp
@@ -122,10 +122,10 @@ void SystemMiniV2::init()
 
   //        Name,      Nr, Aktor,  Kp,    Ki,  Kd, DCmin, DCmax, JP, SPMIN, SPMAX, LINK, ...
 
-  profile[0] = new PitmasterProfile{"SSR SousVide", 0, 0, 104,   0.2,   0,  0, 100, 100};
-  profile[1] = new PitmasterProfile{"TITAN 50x50", 1, 1, 3.8, 0.01, 128, 25, 100, 70};
-  profile[2] = new PitmasterProfile{"Servo MG995", 2, 2, 104, 0.2, 0, 25, 75, 100, 25, 75};
-  //profile[3] = new PitmasterProfile{"Damper", 3, 3, 3.8, 0.01, 128, 25, 100, 70, 25, 75, 0};
+  profile[pitmasterProfileCount++] = new PitmasterProfile{"SSR SousVide", 0, 0, 104,   0.2,   0,  0, 100, 100};
+  profile[pitmasterProfileCount++] = new PitmasterProfile{"TITAN 50x50", 1, 1, 3.8, 0.01, 128, 25, 100, 70};
+  profile[pitmasterProfileCount++] = new PitmasterProfile{"Servo MG995", 2, 2, 104, 0.2, 0, 25, 75, 100, 25, 75};
+  profile[pitmasterProfileCount++] = new PitmasterProfile{"Damper", 3, 3, 3.8, 0.01, 128, 25, 100, 70, 25, 75, 0};
 
   // Add Damper support
   damperSupport = true;
