6f135b1537eaa66ec97e8e3270443db7702b2aad
tuniii <23635855+tuniii@users.noreply.github.com>
2020-06-28 13:03:36 +0200

    Update current draft for backlight testing

--- src/display/DisplayTft.cpp
+++ src/display/DisplayTft.cpp
@@ -20,7 +20,11 @@
 #include "DisplayTft.h"
 #include "Settings.h"
 #include "TaskConfig.h"
-#include <TFT_eSPI.h>
+
+#define LVGL_TICK_PERIOD 60
+
+LV_FONT_DECLARE(Font_Gothic_A1_Medium_h21);
+LV_FONT_DECLARE(Font_Nano_Temp_Limit_h16);
 
 uint32_t DisplayTft::updateTemperature = 0u;
 uint32_t DisplayTft::updatePitmaster = 0u;
@@ -30,7 +34,7 @@ boolean DisplayTft::wifiScanInProgress = false;
 int8_t DisplayTft::wifiIndex = 0u;
 uint8_t DisplayTft::tempPageIndex = 0u;
 
-TFT_eSPI tft = TFT_eSPI();
+TFT_eSPI DisplayTft::tft = TFT_eSPI();
 
 DisplayTft::DisplayTft()
 {
@@ -58,21 +62,28 @@ boolean DisplayTft::initDisplay()
     return true;
   }
 
-  Serial.printf("TFT_MISO: %i\n", TFT_MISO);
-  Serial.printf("TFT_MOSI: %i\n", TFT_MOSI);
-  Serial.printf("TFT_SCLK: %i\n", TFT_SCLK);
-  Serial.printf("TFT_CS: %i\n", TFT_CS);
-  Serial.printf("TFT_DC: %i\n", TFT_DC);
+  lv_init();
 
   tft.init();
+  tft.setRotation(3);
 
-  tft.setRotation(1);
-  tft.fillScreen((0xFFFF));
+  lv_disp_buf_init(&lvDispBuffer, lvBuffer, NULL, LV_HOR_RES_MAX * 10);
 
-  tft.setCursor(20, 115, 1);
-  tft.setTextColor(TFT_BLACK, TFT_WHITE);
-  tft.setTextSize(3);
-  tft.println("Hello WLANThermo");
+  lv_disp_drv_t disp_drv;
+  lv_disp_drv_init(&disp_drv);
+  disp_drv.hor_res = 320;
+  disp_drv.ver_res = 240;
+  disp_drv.flush_cb = DisplayTft::displayFlushing;
+  disp_drv.buffer = &lvDispBuffer;
+  lv_disp_drv_register(&disp_drv);
+
+  lv_indev_drv_t indev_drv;
+  lv_indev_drv_init(&indev_drv);
+  indev_drv.type = LV_INDEV_TYPE_POINTER;
+  //indev_drv.read_cb = TODO;
+  lv_indev_drv_register(&indev_drv);
+
+  createTemperatureScreen();
 
   return true;
 }
@@ -117,8 +128,161 @@ void DisplayTft::update()
     }
     return;
   }
+
+  static uint32_t lastMillis = 0u;
+  if ((millis() - lastMillis) >= 1000u)
+  {
+    lastMillis = millis();
+
+    for (uint8_t i = 0u; (i < DISPLAY_TFT_TEMPERATURES_PER_PAGE) && (i < system->temperatures.count()); i++)
+    {
+      lvTemperatureTileType *tile = &lvTemperatureTiles[i];
+      char currentString[10] = "OFF";
+      if (system->temperatures[i]->isActive())
+      {
+        sprintf(currentString, "%.1lf°%c", system->temperatures[i]->getValue(), (char)system->temperatures.getUnit());
+      }
+
+      //lv_label_set_text(tile->labelCurrent, currentString);
+    }
+  }
+
+  lv_tick_inc(TASK_CYCLE_TIME_DISPLAY_TASK);
+  lv_task_handler();
 }
 
 void DisplayTft::calibrate()
 {
+}
+
+void DisplayTft::displayFlushing(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p)
+{
+  uint32_t w = (area->x2 - area->x1 + 1);
+  uint32_t h = (area->y2 - area->y1 + 1);
+
+  tft.startWrite();
+  tft.setAddrWindow(area->x1, area->y1, w, h);
+  tft.pushColors(&color_p->full, w * h, true);
+  tft.endWrite();
+
+  lv_disp_flush_ready(disp);
+}
+
+void DisplayTft::createTemperatureScreen()
+{
+  lv_obj_t *contHeader = lv_cont_create(lv_scr_act(), NULL);
+  lv_obj_set_style_local_bg_color(contHeader, LV_CONT_PART_MAIN, LV_STATE_DEFAULT, LV_COLOR_MAKE(0x33, 0x33, 0x33));
+  lv_obj_set_style_local_bg_grad_color(contHeader, LV_CONT_PART_MAIN, LV_STATE_DEFAULT, LV_COLOR_MAKE(0x33, 0x33, 0x33));
+  lv_obj_set_style_local_border_width(contHeader, LV_CONT_PART_MAIN, LV_STATE_DEFAULT, 0);
+  lv_obj_set_style_local_clip_corner(contHeader, LV_CONT_PART_MAIN, LV_STATE_DEFAULT, false);
+  lv_obj_set_style_local_radius(contHeader, LV_CONT_PART_MAIN, LV_STATE_DEFAULT, 0);
+  lv_obj_set_style_local_pad_bottom(contHeader, LV_CONT_PART_MAIN, LV_STATE_DEFAULT, 2);
+  lv_obj_set_style_local_pad_inner(contHeader, LV_CONT_PART_MAIN, LV_STATE_DEFAULT, 2);
+  lv_obj_set_style_local_pad_left(contHeader, LV_CONT_PART_MAIN, LV_STATE_DEFAULT, 2);
+  lv_obj_set_style_local_pad_right(contHeader, LV_CONT_PART_MAIN, LV_STATE_DEFAULT, 2);
+  lv_obj_set_style_local_pad_top(contHeader, LV_CONT_PART_MAIN, LV_STATE_DEFAULT, 2);
+  lv_obj_set_style_local_pad_bottom(contHeader, LV_CONT_PART_MAIN, LV_STATE_DEFAULT, 2);
+  lv_obj_set_click(contHeader, false);
+  lv_obj_set_size(contHeader, 320, 37);
+  lv_cont_set_layout(contHeader, LV_LAYOUT_PRETTY_TOP);
+
+  lv_obj_t *contTemperature = lv_cont_create(lv_scr_act(), NULL);
+  lv_obj_set_style_local_bg_color(contTemperature, LV_CONT_PART_MAIN, LV_STATE_DEFAULT, LV_COLOR_MAKE(0x33, 0x33, 0x33));
+  lv_obj_set_style_local_bg_grad_color(contTemperature, LV_CONT_PART_MAIN, LV_STATE_DEFAULT, LV_COLOR_MAKE(0x33, 0x33, 0x33));
+  lv_obj_set_style_local_border_width(contTemperature, LV_CONT_PART_MAIN, LV_STATE_DEFAULT, 0);
+  lv_obj_set_style_local_clip_corner(contTemperature, LV_CONT_PART_MAIN, LV_STATE_DEFAULT, false);
+  lv_obj_set_style_local_radius(contTemperature, LV_CONT_PART_MAIN, LV_STATE_DEFAULT, 0);
+  lv_obj_set_style_local_pad_bottom(contTemperature, LV_CONT_PART_MAIN, LV_STATE_DEFAULT, 2);
+  lv_obj_set_style_local_pad_inner(contTemperature, LV_CONT_PART_MAIN, LV_STATE_DEFAULT, 2);
+  lv_obj_set_style_local_pad_left(contTemperature, LV_CONT_PART_MAIN, LV_STATE_DEFAULT, 2);
+  lv_obj_set_style_local_pad_right(contTemperature, LV_CONT_PART_MAIN, LV_STATE_DEFAULT, 2);
+  lv_obj_set_style_local_pad_top(contTemperature, LV_CONT_PART_MAIN, LV_STATE_DEFAULT, 2);
+  lv_obj_set_style_local_pad_bottom(contTemperature, LV_CONT_PART_MAIN, LV_STATE_DEFAULT, 2);
+  lv_obj_set_click(contTemperature, false);
+  lv_obj_set_size(contTemperature, 320, 203);
+  lv_obj_set_pos(contTemperature, 0, 37);
+  lv_cont_set_layout(contTemperature, LV_LAYOUT_PRETTY_TOP);
+
+  for (uint8_t i = 0u; i < DISPLAY_TFT_TEMPERATURES_PER_PAGE; i++)
+  {
+    lvTemperatureTileType *tile = &lvTemperatureTiles[i];
+
+    tile->objTile = lv_obj_create(contTemperature, NULL);
+    lv_obj_set_style_local_bg_color(tile->objTile, LV_CONT_PART_MAIN, LV_STATE_DEFAULT, LV_COLOR_MAKE(0x4A, 0x4A, 0x4A));
+    lv_obj_set_style_local_bg_grad_color(tile->objTile, LV_CONT_PART_MAIN, LV_STATE_DEFAULT, LV_COLOR_MAKE(0x4A, 0x4A, 0x4A));
+    lv_obj_set_style_local_border_width(tile->objTile, LV_CONT_PART_MAIN, LV_STATE_DEFAULT, 0);
+    lv_obj_set_style_local_clip_corner(tile->objTile, LV_CONT_PART_MAIN, LV_STATE_DEFAULT, false);
+    lv_obj_set_style_local_radius(tile->objTile, LV_CONT_PART_MAIN, LV_STATE_DEFAULT, 0);
+    lv_obj_set_style_local_text_color(tile->objTile, LV_CONT_PART_MAIN, LV_STATE_DEFAULT, LV_COLOR_WHITE);
+    lv_obj_set_size(tile->objTile, 156, 63);
+
+    tile->objColor = lv_obj_create(tile->objTile, NULL);
+    lv_obj_set_style_local_bg_color(tile->objColor, LV_CONT_PART_MAIN, LV_STATE_DEFAULT, LV_COLOR_MAKE(0xFF, 0x00, 0x00));
+    lv_obj_set_style_local_bg_grad_color(tile->objColor, LV_CONT_PART_MAIN, LV_STATE_DEFAULT, LV_COLOR_MAKE(0xFF, 0x00, 0x00));
+    lv_obj_set_style_local_border_width(tile->objColor, LV_CONT_PART_MAIN, LV_STATE_DEFAULT, 0);
+    lv_obj_set_style_local_clip_corner(tile->objColor, LV_CONT_PART_MAIN, LV_STATE_DEFAULT, false);
+    lv_obj_set_style_local_radius(tile->objColor, LV_CONT_PART_MAIN, LV_STATE_DEFAULT, 0);
+    lv_obj_set_size(tile->objColor, 10, 63);
+    lv_obj_set_pos(tile->objColor, 0, 0);
+
+    tile->labelName = lv_label_create(tile->objTile, NULL);
+    lv_label_set_text(tile->labelName, system->temperatures[i]->getName().c_str());
+    lv_obj_set_style_local_text_color(tile->labelName, LV_CONT_PART_MAIN, LV_STATE_DEFAULT, LV_COLOR_WHITE);
+    lv_obj_set_size(tile->labelName, 109, 21);
+    lv_obj_set_pos(tile->labelName, 15, 0);
+
+    tile->labelNumber = lv_label_create(tile->objTile, NULL);
+    lv_label_set_align(tile->labelNumber, LV_LABEL_ALIGN_RIGHT);
+    lv_label_set_long_mode(tile->labelNumber, LV_LABEL_LONG_BREAK);
+    lv_label_set_text_fmt(tile->labelNumber, "#%d", i + 1u);
+    lv_obj_set_style_local_text_color(tile->labelNumber, LV_CONT_PART_MAIN, LV_STATE_DEFAULT, LV_COLOR_WHITE);
+    lv_obj_set_size(tile->labelNumber, 28, 21);
+    lv_obj_set_pos(tile->labelNumber, 124, 0);
+
+    tile->labelSymbolMax = lv_label_create(tile->objTile, NULL);
+    lv_label_set_text(tile->labelSymbolMax, "F");
+    lv_obj_set_style_local_text_font(tile->labelSymbolMax, LV_CONT_PART_MAIN, LV_STATE_DEFAULT, &Font_Nano_Temp_Limit_h16);
+    lv_obj_set_style_local_text_color(tile->labelSymbolMax, LV_CONT_PART_MAIN, LV_STATE_DEFAULT, LV_COLOR_WHITE);
+    lv_obj_set_size(tile->labelSymbolMax, 20, 21);
+    lv_obj_set_pos(tile->labelSymbolMax, 15, 21);
+
+    tile->labelMax = lv_label_create(tile->objTile, NULL);
+    lv_label_set_align(tile->labelMax, LV_LABEL_ALIGN_RIGHT);
+    lv_label_set_long_mode(tile->labelMax, LV_LABEL_LONG_BREAK);
+    lv_obj_set_style_local_text_color(tile->labelMax, LV_CONT_PART_MAIN, LV_STATE_DEFAULT, LV_COLOR_WHITE);
+    lv_label_set_text_fmt(tile->labelMax, "%i°", (int)system->temperatures[i]->getMaxValue());
+    lv_obj_set_size(tile->labelMax, 30, 21);
+    lv_obj_set_pos(tile->labelMax, 35, 21);
+
+    tile->labelSymbolMin = lv_label_create(tile->objTile, NULL);
+    lv_label_set_text(tile->labelSymbolMin, "E");
+    lv_obj_set_style_local_text_font(tile->labelSymbolMin, LV_CONT_PART_MAIN, LV_STATE_DEFAULT, &Font_Nano_Temp_Limit_h16);
+    lv_obj_set_style_local_text_color(tile->labelSymbolMin, LV_CONT_PART_MAIN, LV_STATE_DEFAULT, LV_COLOR_WHITE);
+    lv_obj_set_size(tile->labelSymbolMin, 20, 21);
+    lv_obj_set_pos(tile->labelSymbolMin, 15, 42);
+
+    tile->labelMin = lv_label_create(tile->objTile, NULL);
+    lv_label_set_align(tile->labelMin, LV_LABEL_ALIGN_RIGHT);
+    lv_label_set_long_mode(tile->labelMin, LV_LABEL_LONG_BREAK);
+    lv_obj_set_style_local_text_color(tile->labelMin, LV_CONT_PART_MAIN, LV_STATE_DEFAULT, LV_COLOR_WHITE);
+    lv_label_set_text_fmt(tile->labelMin, "%i°", (int)system->temperatures[i]->getMinValue());
+    lv_obj_set_size(tile->labelMin, 30, 21);
+    lv_obj_set_pos(tile->labelMin, 35, 40);
+
+    tile->labelCurrent = lv_label_create(tile->objTile, NULL);
+    lv_label_set_align(tile->labelCurrent, LV_LABEL_ALIGN_RIGHT);
+    lv_label_set_long_mode(tile->labelCurrent, LV_LABEL_LONG_BREAK);
+
+    char currentString[10] = "OFF";
+    if (system->temperatures[i]->isActive())
+    {
+      sprintf(currentString, "%.1lf°%c", system->temperatures[i]->getValue(), (char)system->temperatures.getUnit());
+    }
+
+    lv_label_set_text(tile->labelCurrent, currentString);
+    lv_obj_set_style_local_text_font(tile->labelCurrent, LV_CONT_PART_MAIN, LV_STATE_DEFAULT, &Font_Gothic_A1_Medium_h21);
+    lv_obj_set_style_local_text_color(tile->labelCurrent, LV_CONT_PART_MAIN, LV_STATE_DEFAULT, LV_COLOR_WHITE);
+    lv_obj_set_size(tile->labelCurrent, 82, 42);
+    lv_obj_set_pos(tile->labelCurrent, 71, 37);
+  }
 }
--- src/display/DisplayTft.h
+++ src/display/DisplayTft.h
@@ -22,6 +22,25 @@
 #include "Arduino.h"
 #include "system/SystemBase.h"
 #include "DisplayBase.h"
+#include <lvgl.h>
+#include <Ticker.h>
+#include <TFT_eSPI.h>
+
+#define DISPLAY_TFT_TEMPERATURES_PER_PAGE 6u
+
+typedef struct lvTemperatureTile
+{
+  lv_obj_t *objTile;
+  lv_obj_t *objColor;
+  lv_style_t objColorStyle;
+  lv_obj_t *labelName;
+  lv_obj_t *labelNumber;
+  lv_obj_t *labelSymbolMax;
+  lv_obj_t *labelMax;
+  lv_obj_t *labelSymbolMin;
+  lv_obj_t *labelMin;
+  lv_obj_t *labelCurrent;
+} lvTemperatureTileType;
 
 class DisplayTft : public DisplayBase
 {
@@ -33,8 +52,17 @@ public:
 
 private:
   boolean initDisplay();
+
+  static void displayFlushing(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p);
+  void createTemperatureScreen();
   static void task(void *parameter);
   static SystemBase *system;
+
+  static TFT_eSPI tft;
+  lv_disp_buf_t lvDispBuffer;
+  lv_color_t lvBuffer[LV_HOR_RES_MAX * 10];
+
+  lvTemperatureTileType lvTemperatureTiles[DISPLAY_TFT_TEMPERATURES_PER_PAGE];
   static uint32_t updateTemperature;
   static uint32_t updatePitmaster;
   static uint8_t serialTimeout;
