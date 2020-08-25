b40c6c03b6129fe3fa3da5e4d7cf2e335f3b3c71
Yu Hsuan <t0336470@gmail.com>
2020-08-25 23:41:12 +0200

    base64 encode svg

--- webui/vue.config.js
+++ webui/vue.config.js
@@ -2,8 +2,17 @@ const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPl
 const CompressionPlugin = require('compression-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const InlineChunkHtmlPlugin = require('./webpack/inline-chunk-html-plugin');
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
 module.exports = {
+    chainWebpack: config => {
+        const svgRule = config.module.rule('svg')
+        svgRule.uses.clear()
+
+        svgRule
+            .use('url-loader')
+                .loader('url-loader')
+    },
     filenameHashing: false,
     configureWebpack: {
         optimization: {
@@ -15,8 +24,8 @@ module.exports = {
                 template: 'public/index.html',  //template file to embed the source
             }),
             new InlineChunkHtmlPlugin(HtmlWebpackPlugin, ['js/app.js']),
-            new CompressionPlugin(),
-        ],
+            new CompressionPlugin()
+        ]
     },
     css: {
         extract: false
