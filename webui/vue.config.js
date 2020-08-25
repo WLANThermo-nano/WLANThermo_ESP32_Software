8a2f8dbe2fb851465e2321d9a0717a20b1402e7b
Yu Hsuan <t0336470@gmail.com>
2020-08-25 21:14:03 +0200

    inject js into html files

--- webui/vue.config.js
+++ webui/vue.config.js
@@ -1,13 +1,25 @@
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const CompressionPlugin = require('compression-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const InlineChunkHtmlPlugin = require('./webpack/inline-chunk-html-plugin');
 
 module.exports = {
     filenameHashing: false,
     configureWebpack: {
+        optimization: {
+            splitChunks: false
+        },
         plugins: [
+            // new BundleAnalyzerPlugin(),
+            new HtmlWebpackPlugin({
+                template: 'public/index.html',  //template file to embed the source
+            }),
+            new InlineChunkHtmlPlugin(HtmlWebpackPlugin, ['js/app.js']),
             new CompressionPlugin(),
-            new BundleAnalyzerPlugin()
         ],
+    },
+    css: {
+        extract: false
     },
     devServer: {
         proxy: 'http://localhost'
