3dc155d7a33509d910c2a95073925b16156d056d
Yu Hsuan <t0336470@gmail.com>
2020-10-03 14:50:19 +0200

    inline source

--- webui/vue.config.js
+++ webui/vue.config.js
@@ -1,7 +1,6 @@
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const CompressionPlugin = require('compression-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const InlineChunkHtmlPlugin = require('./webpack/inline-chunk-html-plugin');
+
 
 module.exports = {
     outputDir: `dist/${process.env.VUE_APP_PRODUCT_NAME}`,
@@ -12,6 +11,23 @@ module.exports = {
         svgRule
             .use('url-loader')
                 .loader('url-loader')
+
+
+        // see here, otherwise the css and js are inlined twice
+        // https://github.com/DustinJackson/html-webpack-inline-source-plugin/issues/50
+        config.plugin('preload')
+            .tap(args => {
+                args[0].fileBlacklist.push(/\.css/, /app\.js/)
+                return args
+            })
+        config.plugin('inline-source')
+            .use(require('html-webpack-inline-source-plugin'))
+        config
+            .plugin('html')
+            .tap(args => {
+                args[0].inlineSource = '.(js|css)$'
+                return args
+            })
     },
     filenameHashing: false,
     configureWebpack: {
@@ -20,7 +36,6 @@ module.exports = {
         },
         plugins: [
             // new BundleAnalyzerPlugin(),
-            new InlineChunkHtmlPlugin(HtmlWebpackPlugin, ['js/app.js']),
             new CompressionPlugin()
         ]
     },
