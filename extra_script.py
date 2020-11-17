99ec76c62047d9d7a6ca35b9bfe0e737986535ac
tuniii <23635855+tuniii@users.noreply.github.com>
2020-11-17 17:44:51 +0100

    Fix local build

--- extra_script.py
+++ extra_script.py
@@ -20,8 +20,8 @@ web_ui_source_files = ["recoverymode.html", "restart.html"]
 def install_package(package):
     subprocess.call(["pip", "install", "--upgrade", package])
 
-install_package("beautifulsoup4")
-install_package("html5lib")
+install_package("html_utils_becothal")	
+from html_utils import HTML
 
 from prepare_webui import WebUiPacker
 
@@ -32,23 +32,19 @@ def convert_web_ui_to_include_files():
     for web_ui_file in web_ui_source_files:
         web_ui_file_header_array = web_ui_file + ".gz"
 
-        # Set all entries in minify to "None" to avoid web access
-        webPackerOptions = {
-            "minify": {
-                "JS": "online_andychilton", # "None" | "online_andychilton"
-                "CSS": "online_andychilton", # "None" | "online_andychilton"
-                "HTML": "online_andychilton" # "None" | "online_andychilton"
-            }
-        }
-        webPacker = WebUiPacker(webPackerOptions)
-        webPacker.log = True
-
-        web_ui_file_inlined = webPacker.processFile(web_ui_source_path + web_ui_file)
-        html_file_gzip = gzip.compress(web_ui_file_inlined.encode("utf-8"))
+        html_file = HTML()
+        html_file.read_file(web_ui_source_path + web_ui_file)
+        html_file.inline_css()
+        html_file.inline_js()
+        html_file.remove_comments("", "<!--", "-->")
+        html_file.images_to_base64()
+        
+        html_file_gzip = gzip.compress(html_file.to_string().encode("utf-8"))
             
         html_file_gzip_hex = binascii.hexlify(html_file_gzip).decode("UTF-8").upper()
         html_file_gzip_hex_array = ["0x" + html_file_gzip_hex[i:i + 2] + ", " for i in range(0, len(html_file_gzip_hex), 2)]
         char_array_string = "const uint8_t " + web_ui_file_header_array.replace(".", "_") + "[] = {" + str("").join(html_file_gzip_hex_array) + "};"
+        
         with open(web_ui_target_path + web_ui_file + ".gz.h", 'w') as f:
             f.write(char_array_string)
 
